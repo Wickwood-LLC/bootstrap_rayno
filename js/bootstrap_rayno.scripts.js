d7ed7e7c53414a46692f7b570e06740eb93b55f9
palagpatski <palagpatski@gmail.com>
2020-11-09 22:25:27 +0800

    Fix script

--- js/bootstrap_rayno.scripts.js
+++ js/bootstrap_rayno.scripts.js
@@ -18,7 +18,7 @@
       $(window).on("resize mresize", sticky);
 
       function sticky() {
-        topSpacing = $('#toolbar-bar').length.height() + $('.toolbar-tray-horizontal.is-active').length.height();
+        topSpacing = (isNaN($('#toolbar-bar').height())) ? 0 : $('#toolbar-bar').height() + (isNaN($('.toolbar-tray-horizontal.is-active').height())) ? 0 : $('.toolbar-tray-horizontal.is-active').height();
         // headerWidth = $header.width(); // gets the width of the container
         headerHeight = $header.height(); // gets the height of our header
         $header.css({
