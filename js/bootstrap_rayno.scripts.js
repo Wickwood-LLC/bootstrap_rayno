9b886ae44464839145a100d00732be9bf19f72ee
palagpatski <palagpatski@gmail.com>
2020-11-16 13:12:32 +0800

    Mobile menu parent item should not remain clickable, else we can't open the submenus

--- js/bootstrap_rayno.scripts.js
+++ js/bootstrap_rayno.scripts.js
@@ -17,40 +17,40 @@
       $(document).ready(delaySticky);
       $(window).on("resize mresize", delaySticky);
 
-        function delaySticky() {
-            setTimeout(function sticky() {
-              if ($('sticky-header')) {
-                $header.removeClass('sticky-header');
-              }
-              topSpacing = ((isNaN($('.toolbar-fixed #toolbar-bar').height())) ? 0 : $('#toolbar-bar').height()) + ((isNaN($('.toolbar-tray-horizontal.is-active').height())) ? 0 : $('.toolbar-tray-horizontal.is-active').height());
-              // headerWidth = $header.width(); // gets the width of the container
-              headerHeight = $header.height(); // gets the height of our header
-              $(".header-container").css("height", headerHeight);
-              $header.css({
-                // width: "initial",
-              });
-              
-              // headerHeight = $header.height(); // gets the height of our header
-      
-              stickyTop = $header.offset().top; // tells how far our target element is from the top of the page
-              windowTop = $(window).scrollTop(); // tells how far our screen is currently from the top of the page
-              currentPosition = stickyTop - windowTop - topSpacing + headerHeight; // tells how far our target element is from where our screen is currently
-      
-              // console.log('Distance from top of page: ' + stickyTop);
-              // console.log('Position on load ' + currentPosition);
-      
-      
-              if (currentPosition < 0) { // if target element goes above the screen
-                $header.addClass('sticky-header');
-                $header.css("top", topSpacing);
-                $header.slideDown();
-              } else {
-                $header.removeClass('sticky-header');
-                $header.css("top", "");
-              }
-              // console.log("Top spacing is " + topSpacing);
-            }, 500);
-        }
+      function delaySticky() {
+        setTimeout(function sticky() {
+          if ($('sticky-header')) {
+            $header.removeClass('sticky-header');
+          }
+          topSpacing = ((isNaN($('.toolbar-fixed #toolbar-bar').height())) ? 0 : $('#toolbar-bar').height()) + ((isNaN($('.toolbar-tray-horizontal.is-active').height())) ? 0 : $('.toolbar-tray-horizontal.is-active').height());
+          // headerWidth = $header.width(); // gets the width of the container
+          headerHeight = $header.height(); // gets the height of our header
+          $(".header-container").css("height", headerHeight);
+          $header.css({
+            // width: "initial",
+          });
+
+          // headerHeight = $header.height(); // gets the height of our header
+
+          stickyTop = $header.offset().top; // tells how far our target element is from the top of the page
+          windowTop = $(window).scrollTop(); // tells how far our screen is currently from the top of the page
+          currentPosition = stickyTop - windowTop - topSpacing + headerHeight; // tells how far our target element is from where our screen is currently
+
+          // console.log('Distance from top of page: ' + stickyTop);
+          // console.log('Position on load ' + currentPosition);
+
+
+          if (currentPosition < 0) { // if target element goes above the screen
+            $header.addClass('sticky-header');
+            $header.css("top", topSpacing);
+            $header.slideDown();
+          } else {
+            $header.removeClass('sticky-header');
+            $header.css("top", "");
+          }
+          // console.log("Top spacing is " + topSpacing);
+        }, 500);
+      }
 
       $(window).scroll(function() { // scroll event 
         windowTop = $(window).scrollTop(); // tells how far our screen is currently from the top of the page
@@ -76,9 +76,11 @@
    */
   Drupal.behaviors.clickMenu = {
     attach: function(context, settings) {
+      if ($(window).width() > 546) {
         $('.menu .dropdown > a').click(function() {
           location.href = this.href;
         });
+      }
     }
   };
 
